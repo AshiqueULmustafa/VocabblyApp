cdac2835d881310b52c7c23ac651929e1f7b016e
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-12-29 19:46:39 +0600

    created home view

---
+++ app/src/main/java/com/ashique/vocabblyapp/VocabblyHome.kt
@@ -0,0 +1,16 @@
+package com.ashique.vocabblyapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VocabblyHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_vocabbly_home)
+
+    }
+}
